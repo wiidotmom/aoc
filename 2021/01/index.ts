f1436cc7cbb62f285f0e2e9e25965b24a3d5814b
iGalaxyYT <contact@igalaxy.dev>
2021-12-01 13:56:56 -0800

    2021 Day 1 solution

--- 2021/01/index.ts
+++ 2021/01/index.ts
@@ -1,2 +1,42 @@
 import fs from 'fs';
+import path from 'path';
 
+const INPUT_FILE = fs
+	.readFileSync(path.join(__dirname, 'input.txt'))
+	.toString();
+
+const findSolutionOne = (input: number[]): number => {
+	let inc: number = 0;
+	let previous: number;
+
+	input.forEach(x => {
+		if (x > previous) inc++;
+		previous = x;
+	});
+
+	return inc;
+};
+
+const findSolutionTwo = (input: number[]): number => {
+	let inc: number = 0;
+	let window: number[] = input.slice(0, 2);
+	let previousSum: number;
+
+	input.slice(2).forEach(x => {
+		if (window.length === 3) window = window.slice(1);
+
+		window.push(x);
+
+		const sum = window.reduce((a, b) => a + b);
+
+		if (sum > previousSum) inc++;
+
+		previousSum = sum;
+	});
+
+	return inc;
+};
+
+let data = INPUT_FILE.split('\n').map(x => Number(x));
+console.log(`Solution 1: ${findSolutionOne(data)}`);
+console.log(`Solution 2: ${findSolutionTwo(data)}`);
