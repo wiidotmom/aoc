c2b03cd516afe48b2dfe40e899ff0bf74df4b7a3
iGalaxyYT <contact@igalaxy.dev>
2021-12-02 12:13:33 -0800

    rust 2021 day 01

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,16 +4,36 @@ use std::process::Command;
 fn main() {
 	let args: Vec<String> = env::args().collect();
 
+	println!("Emptying temp dir...");
+
+	let output = Command::new("rm")
+		.arg("-rf")
+		.arg("temp")
+		.output()
+		.expect("Failed to empty temp directory");
+
+	println!("{}", String::from_utf8(output.stdout).unwrap());
+
+	println!("Creating new temp dir...");
+
+	let output0 = Command::new("mkdir")
+		.arg("temp")
+		.output()
+		.expect("Failed to create temp directory");
+
+	println!("{}", String::from_utf8(output0.stdout).unwrap());
+
 	println!("Building...");
 
-	let output = Command::new("rustc")
+	let output1 = Command::new("rustc")
 		.arg(format!("{}/main.rs", args[1]))
 		.arg("--out-dir")
 		.arg("temp")
 		.output()
 		.expect("Failed to build");
 
-	println!("{}", String::from_utf8(output.stdout).unwrap());
+	println!("{}", String::from_utf8(output1.stderr).unwrap());
+	println!("{}", String::from_utf8(output1.stdout).unwrap());
 
 	println!("Copying input to temp directory...");
 
