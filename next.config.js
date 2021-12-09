49248af1831c9d87b6d2b869a13ccf2938c68eec
iGalaxyYT <contact@igalaxy.dev>
2021-12-09 14:42:25 -0800

    fix(misc): redirects

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
 	experimental: {
 		styledComponents: true,
 	},
+	async redirects() {
+		return [
+			{
+				source: '/',
+				destination: '/2021/01',
+				permanent: false,
+			},
+		];
+	},
 };
