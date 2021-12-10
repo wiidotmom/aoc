c94edab612cde8a5f979278fe40f06f895a1b19c
iGalaxyYT <contact@igalaxy.dev>
2021-12-09 17:46:43 -0800

    2021 day 09

--- src/solutions/2021/09/index.ts
+++ src/solutions/2021/09/index.ts
@@ -2,37 +2,53 @@ import { createIntegerGridFromString, Grid } from 'utils';
 
 import input from './input';
 
-const getBasin = (grid: Grid<number>, pair: number[]) => {
-	const array = grid.toArray();
-	const [x, y] = pair;
+const getAdjacentPoints = (array: number[][], [x, y]: number[]) => {
+	return [
+		[-1, 0],
+		[1, 0],
+		[0, -1],
+		[0, 1],
+	]
+		.map(([addX, addY]) => [x + addX, y + addY])
+		.filter(
+			([x, y]) => x >= 0 && y >= 0 && y < array.length && x < array[0].length
+		);
+};
 
-	let basin: number[][] = [];
+const getLowPoints = (array: number[][]) => {
+	let lowPoints: number[][] = [];
+
+	for (let y = 0; y < array.length; y++) {
+		for (let x = 0; x < array[0].length; x++) {
+			if (
+				getAdjacentPoints(array, [x, y]).every(
+					([ax, ay]) => array[ay][ax] > array[y][x]
+				)
+			)
+				lowPoints.push([x, y]);
+		}
+	}
+
+	return lowPoints;
+};
+
+const getBasin = (array: number[][], originalPoint: number[]) => {
+	/**
+	 * Sets can only contain unique values \
+	 * Storing in strings as arrays would be considered unique \
+	 * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+	 */
+	const basin = new Set<string>();
 
 	const getWalls = (pair: number[]) => {
-		let walls: number[][] = [];
-		[
-			[x - 1, y],
-			[x + 1, y],
-			[x, y - 1],
-			[x, y + 1],
-		].forEach(pair => {
-			if (
-				pair[0] >= 0 &&
-				pair[0] < array[0].length &&
-				pair[1] >= 0 &&
-				pair[1] < array.length
+		basin.add(`${pair[0]},${pair[1]}`);
+		getAdjacentPoints(array, pair)
+			.filter(
+				([x, y]) => array[y][x] < 9 && array[y][x] > array[pair[1]][pair[0]]
 			)
-				walls.push(pair);
-		});
-		walls = walls.filter(
-			([wx, wy]) => grid.get(wx, wy) < 9 && grid.get(wx, wy) > grid.get(x, y)
-		);
-		basin.push(...walls);
-		console.log(walls);
-		walls.forEach(wall => getWalls(wall));
+			.forEach(getWalls);
 	};
-
-	getWalls(pair);
+	getWalls(originalPoint);
 
 	return basin;
 };
@@ -42,79 +58,22 @@ export const parseInput = () => input;
 export const findSolutionOne = (input: string): number => {
 	const grid = createIntegerGridFromString(input);
 
-	const array = grid.toArray();
-
-	let total: number = 0;
-
-	for (let y = 0; y < array.length; y++) {
-		for (let x = 0; x < array[0].length; x++) {
-			const center = grid.get(x, y);
-
-			let toCheck: number[] = [];
-
-			[
-				[x - 1, y],
-				[x + 1, y],
-				[x, y - 1],
-				[x, y + 1],
-			].forEach(pair => {
-				if (
-					pair[0] >= 0 &&
-					pair[0] < array[0].length &&
-					pair[1] >= 0 &&
-					pair[1] < array.length
-				)
-					toCheck.push(grid.get(pair[0], pair[1]));
-			});
-
-			if (toCheck.every(x => x > center)) total += center + 1;
-		}
-	}
-
-	return total;
+	return [...getLowPoints(grid.toArray())]
+		.map(([x, y]) => grid.get(x, y) + 1)
+		.reduce((a, b) => a + b);
 };
 
 export const findSolutionTwo = (input: string): number => {
 	const grid = createIntegerGridFromString(input);
-
 	const array = grid.toArray();
 
-	let lowPoints: number[][] = [];
+	const lowPoints = [...getLowPoints(array)];
 
-	for (let y = 0; y < array.length; y++) {
-		for (let x = 0; x < array[0].length; x++) {
-			const center = grid.get(x, y);
-
-			let toCheck: number[][] = [];
-
-			[
-				[x - 1, y],
-				[x + 1, y],
-				[x, y - 1],
-				[x, y + 1],
-			].forEach(pair => {
-				if (
-					pair[0] >= 0 &&
-					pair[0] < array[0].length &&
-					pair[1] >= 0 &&
-					pair[1] < array.length
-				)
-					toCheck.push([pair[0], pair[1]]);
-			});
-
-			if (toCheck.every(pair => grid.get(pair[0], pair[1]) > center))
-				lowPoints.push([x, y]);
-		}
-	}
-
-	const basins = lowPoints.map(pair => getBasin(grid, pair));
+	const basins = lowPoints.map(pair => getBasin(array, pair));
 
 	return basins
-		.map(basin => basin.length)
+		.map(basin => basin.size)
 		.sort((a, b) => a - b)
 		.slice(-3)
 		.reduce((a, b) => a * b);
 };
-
-console.log(`Solution 1: ${findSolutionOne(parseInput())}`);
-console.log(`Solution 2: ${findSolutionTwo(parseInput())}`);
