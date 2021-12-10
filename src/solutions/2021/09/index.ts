23013bd885e7f8fe326ad779624cdac5ff77e11e
iGalaxyYT <contact@igalaxy.dev>
2021-12-09 16:55:39 -0800

    misc: sync changes

--- src/solutions/2021/09/index.ts
+++ src/solutions/2021/09/index.ts
@@ -2,6 +2,41 @@ import { createIntegerGridFromString, Grid } from 'utils';
 
 import input from './input';
 
+const getBasin = (grid: Grid<number>, pair: number[]) => {
+	const array = grid.toArray();
+	const [x, y] = pair;
+
+	let basin: number[][] = [];
+
+	const getWalls = (pair: number[]) => {
+		let walls: number[][] = [];
+		[
+			[x - 1, y],
+			[x + 1, y],
+			[x, y - 1],
+			[x, y + 1],
+		].forEach(pair => {
+			if (
+				pair[0] >= 0 &&
+				pair[0] < array[0].length &&
+				pair[1] >= 0 &&
+				pair[1] < array.length
+			)
+				walls.push(pair);
+		});
+		walls = walls.filter(
+			([wx, wy]) => grid.get(wx, wy) < 9 && grid.get(wx, wy) > grid.get(x, y)
+		);
+		basin.push(...walls);
+		console.log(walls);
+		walls.forEach(wall => getWalls(wall));
+	};
+
+	getWalls(pair);
+
+	return basin;
+};
+
 export const parseInput = () => input;
 
 export const findSolutionOne = (input: string): number => {
@@ -40,14 +75,15 @@ export const findSolutionOne = (input: string): number => {
 };
 
 export const findSolutionTwo = (input: string): number => {
-	const originalGrid = createIntegerGridFromString(input);
+	const grid = createIntegerGridFromString(input);
 
-	const flowGrid = new Grid<number[][]>([]);
+	const array = grid.toArray();
 
-	const originalArray = originalGrid.toArray();
-	for (let y = 0; y < originalArray.length; y++) {
-		for (let x = 0; x < originalArray[0].length; x++) {
-			const center = originalGrid.get(x, y);
+	let lowPoints: number[][] = [];
+
+	for (let y = 0; y < array.length; y++) {
+		for (let x = 0; x < array[0].length; x++) {
+			const center = grid.get(x, y);
 
 			let toCheck: number[][] = [];
 
@@ -59,14 +95,26 @@ export const findSolutionTwo = (input: string): number => {
 			].forEach(pair => {
 				if (
 					pair[0] >= 0 &&
-					pair[0] < originalArray[0].length &&
+					pair[0] < array[0].length &&
 					pair[1] >= 0 &&
-					pair[1] < originalArray.length
+					pair[1] < array.length
 				)
 					toCheck.push([pair[0], pair[1]]);
 			});
+
+			if (toCheck.every(pair => grid.get(pair[0], pair[1]) > center))
+				lowPoints.push([x, y]);
 		}
 	}
 
-	return 0;
+	const basins = lowPoints.map(pair => getBasin(grid, pair));
+
+	return basins
+		.map(basin => basin.length)
+		.sort((a, b) => a - b)
+		.slice(-3)
+		.reduce((a, b) => a * b);
 };
+
+console.log(`Solution 1: ${findSolutionOne(parseInput())}`);
+console.log(`Solution 2: ${findSolutionTwo(parseInput())}`);
