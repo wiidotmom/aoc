24927982906b74f5fa4dc0791eedc551cdf188b9
iGalaxy <hey@igalaxy.dev>
2022-11-28 14:07:39 -0800

    feat: 2018 day 01

--- src/solutions/2018/01/index.ts
+++ src/solutions/2018/01/index.ts
@@ -9,3 +9,20 @@ export const findSolutionOne = (input: string[]): number => {
 
 	return sum(...nums);
 };
+
+export const findSolutionTwo = (input: string[]): number => {
+	const nums = input.map(x => parseInt(x.replace('+', '')));
+
+	const frequencies = new Map<number, number>();
+
+	let currentFreq = 0;
+	while (frequencies.get(currentFreq) != 2) {
+		nums.forEach(num => {
+			if (frequencies.get(currentFreq) == 2) return;
+			currentFreq += num;
+			if (!frequencies.has(currentFreq)) frequencies.set(currentFreq, 0);
+			frequencies.set(currentFreq, frequencies.get(currentFreq)! + 1);
+		});
+	}
+	return currentFreq;
+};
