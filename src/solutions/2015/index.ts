a2160992f8d825514a9b1a7d5f649e3a57cf9d17
iGalaxy <hey@igalaxy.dev>
2022-11-29 13:11:27 -0800

    feat: 2015 day 02

--- src/solutions/2015/index.ts
+++ src/solutions/2015/index.ts
@@ -1,7 +1,8 @@
 import { Solution } from 'solutions';
 
 import * as day1 from './01';
+import * as day2 from './02';
 
-const solutions: Solution[] = [day1];
+const solutions: Solution[] = [day1, day2];
 
 export default solutions;
