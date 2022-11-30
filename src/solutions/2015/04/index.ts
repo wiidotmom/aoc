6d881f71a97ce768e4f884942628f45a97e43b82
iGalaxy <hey@igalaxy.dev>
2022-11-30 13:24:28 -0800

    feat: 2015 day 04

---
+++ src/solutions/2015/04/index.ts
@@ -0,0 +1,33 @@
+import input from './input';
+
+import * as crypto from 'crypto';
+
+export const parseInput = () => input;
+
+export const findSolutionOne = (input: string): number => {
+	let answer = -1,
+		currentNum = 0;
+	while (answer == -1) {
+		let hash = crypto
+			.createHash('md5')
+			.update(`${input}${currentNum}`)
+			.digest('hex');
+		if (hash.startsWith('00000')) answer = currentNum;
+		currentNum++;
+	}
+	return answer;
+};
+
+export const findSolutionTwo = (input: string): number => {
+	let answer = -1,
+		currentNum = 0;
+	while (answer == -1) {
+		let hash = crypto
+			.createHash('md5')
+			.update(`${input}${currentNum}`)
+			.digest('hex');
+		if (hash.startsWith('000000')) answer = currentNum;
+		currentNum++;
+	}
+	return answer;
+};
