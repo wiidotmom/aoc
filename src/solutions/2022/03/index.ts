f87c25b852540f87b03ae029f2c1b39f71377753
iGalaxyYT <contact@igalaxy.dev>
2022-12-02 21:34:52 -0800

    chore: 2022 day 03 cleanup

--- src/solutions/2022/03/index.ts
+++ src/solutions/2022/03/index.ts
@@ -7,33 +7,27 @@ export const parseInput = () => input.split('\n');
 const getPriority = (item: string) =>
 	'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.indexOf(item) + 1;
 
-export const findSolutionOne = (input: ReturnType<typeof parseInput>) => {
-	const shared: string[] = [];
+export const findSolutionOne = (input: ReturnType<typeof parseInput>) =>
+	sum(
+		...input
+			.map(x => [
+				x.substring(0, x.length / 2),
+				x.substring(x.length / 2, x.length),
+			])
+			.map(rucksack => [...rucksack[0]].find(x => rucksack[1].includes(x))!)
+			.map(x => getPriority(x))
+	);
 
-	input
-		.map(x => [
-			x.substring(0, x.length / 2),
-			x.substring(x.length / 2, x.length),
-		])
-		.forEach(rucksack => {
-			shared.push([...rucksack[0]].find(x => rucksack[1].includes(x))!);
-		});
-
-	return sum(...shared.map(x => getPriority(x)));
-};
-
-export const findSolutionTwo = (input: ReturnType<typeof parseInput>) => {
-	let shared: string[] = [];
-
-	for (let i = 2; i < input.length; i += 3) {
-		let rucksacks = [input[i], input[i - 1], input[i - 2]];
-
-		shared.push(
-			rucksacks[0]
-				.split('')
-				.find(x => rucksacks[1].includes(x) && rucksacks[2].includes(x))!
-		);
-	}
-
-	return sum(...shared.map(x => getPriority(x)));
-};
+export const findSolutionTwo = (input: ReturnType<typeof parseInput>) =>
+	sum(
+		...input
+			.map((x, i) =>
+				(i + 1) % 3 === 0
+					? x
+							.split('')
+							.find(y => input[i - 1].includes(y) && input[i - 2].includes(y))!
+					: undefined
+			)
+			.filter(x => x !== undefined)
+			.map(x => getPriority(x!))
+	);
