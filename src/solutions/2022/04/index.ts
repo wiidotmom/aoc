edd2b9183111252760a2b7dd642dbd498f6f11c2
iGalaxyYT <contact@igalaxy.dev>
2022-12-03 21:13:21 -0800

    feat: 2022 day 04

---
+++ src/solutions/2022/04/index.ts
@@ -0,0 +1,20 @@
+import input from './input';
+
+export const parseInput = () =>
+	input.split('\n').map(x =>
+		x.split(',').map(y =>
+			Array.from(Array(Math.max(+y.split('-')[1])).keys())
+				.map(z => z + 1)
+				.filter(z => z >= +y.split('-')[0])
+		)
+	);
+
+export const findSolutionOne = (input: ReturnType<typeof parseInput>) =>
+	input.filter(x =>
+		x[0].every(y => x[1].includes(y) || x[1].every(y => x[0].includes(y)))
+	).length;
+
+export const findSolutionTwo = (input: ReturnType<typeof parseInput>) =>
+	input.filter(x =>
+		x[0].some(y => x[1].includes(y) || x[1].some(y => x[0].includes(y)))
+	).length;
