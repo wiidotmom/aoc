1424b2fe9a60d515dffb516fa91de4037cc4c80b
iGalaxyYT <contact@igalaxy.dev>
2022-12-08 22:32:23 -0800

    feat: pages

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -17,13 +17,15 @@ export default function RootLayout({
 						<div style={{ marginLeft: '1em' }}>
 							<NavLink href="/about">[About]</NavLink>
 							<NavLink href="/events">[Events]</NavLink>
+							<span style={{ margin: '0 1em' }}>iGalaxy</span>
+							<span style={{ color: '#ffff66' }}>18*</span>
 						</div>
 					</div>
 					<div style={{ display: 'flex' }}>
 						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 						<ImportantLink href="/">2022</ImportantLink>
 						<div style={{ marginLeft: '1em' }}>
-							<NavLink href="/calendar">[Calendar]</NavLink>
+							<NavLink href="/2022">[Calendar]</NavLink>
 							<NavLink href="/leaderboard">[Leaderboard]</NavLink>
 						</div>
 					</div>
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
 		return [
 			{
 				source: '/',
-				destination: '/2022/01',
+				destination: '/2022',
 				permanent: false,
 			},
 		];
