fb4086d5924460280aafbb29eae4576f86349ccc
Mallory Hayr <hey@igalaxy.dev>
2023-12-01 09:27:46 -0800

    chore: refactor 2023 day 01

--- src/solutions/2023/01/index.ts
+++ src/solutions/2023/01/index.ts
@@ -18,59 +18,56 @@ export const findSolutionOne = (
 export const findSolutionTwo = (
 	input: ReturnType<typeof parseInput>
 ): number => {
-	const digits: { [key: string]: number } = {
-		one: 1,
-		two: 2,
-		three: 3,
-		four: 4,
-		five: 5,
-		six: 6,
-		seven: 7,
-		eight: 8,
-		nine: 9,
-	};
+	const digits = [
+		'one',
+		'two',
+		'three',
+		'four',
+		'five',
+		'six',
+		'seven',
+		'eight',
+		'nine',
+	];
 
 	return sum(
 		...input.map(x => {
 			const digs = x.split('').filter(y => !Number.isNaN(+y));
 			let firstDig = digs[0];
 			let lastDig = digs[digs.length - 1];
-			if (
-				Object.keys(digits)
-					.map(y => x.includes(y))
-					.some(y => y)
-			) {
-				const minWordDigIndex = Math.min(
-					...Object.keys(digits)
-						.map(y => x.indexOf(y))
-						.map(y => (y == -1 ? Number.MAX_SAFE_INTEGER : y))
-				);
 
-				if (minWordDigIndex < x.indexOf(firstDig) || firstDig == undefined) {
-					firstDig =
-						digits[
-							Object.keys(digits)[
-								Object.keys(digits)
-									.map(y => x.indexOf(y))
-									.map(y => (y == -1 ? Number.MAX_SAFE_INTEGER : y))
-									.indexOf(minWordDigIndex)
-							]
-						].toString();
+			if (digits.some(y => x.includes(y))) {
+				const firstWord = x.match(new RegExp(`/${digits.join('|')}/`));
+				if (firstWord && firstWord.length > 0) {
+					if (
+						x.indexOf(firstWord[0]) < x.indexOf(firstDig) ||
+						firstDig == undefined
+					)
+						firstDig = (digits.indexOf(firstWord[0]) + 1).toString();
 				}
-				const maxWordDigIndex = Math.max(
-					...Object.keys(digits).map(y => x.lastIndexOf(y))
-				);
-				if (maxWordDigIndex > x.lastIndexOf(lastDig) || lastDig == undefined) {
-					lastDig =
-						digits[
-							Object.keys(digits)[
-								Object.keys(digits)
-									.map(y => x.lastIndexOf(y))
-									.indexOf(maxWordDigIndex)
-							]
-						].toString();
+
+				const lastWord = x
+					.split('')
+					.reverse()
+					.join('')
+					.match(
+						new RegExp(
+							`/${digits.map(y => y.split('').reverse().join('')).join('|')}/`
+						)
+					);
+				if (lastWord && lastWord.length > 0) {
+					if (
+						x.lastIndexOf(lastWord[0].split('').reverse().join('')) >
+							x.lastIndexOf(lastDig) ||
+						lastDig == undefined
+					) {
+						lastDig = (
+							digits.indexOf(lastWord[0].split('').reverse().join('')) + 1
+						).toString();
+					}
 				}
 			}
+
 			return +(firstDig + lastDig);
 		})
 	);
